bf038e3f647fc5504ff96cbb58fb5640d5b91a53
aytal <kitecat.kitekat@gmail.com>
2021-04-28 08:57:54 +0900

    Сделал пример 2

---
+++ src/sample/Controller.java
@@ -0,0 +1,34 @@
+package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Button buttonOne;
+
+    @FXML
+    private Button buttonTwo;
+
+    @FXML
+    private Button buttonThree;
+
+    @FXML
+    private Label labelForButtons;
+
+    @FXML
+    private void clickButton1(ActionEvent actionEvent) {
+        labelForButtons.setText("Clicked Button1");
+    }
+
+    @FXML
+    private void clickButton2(ActionEvent actionEvent) {
+        labelForButtons.setText("Clicked Button2");
+    }
+
+    @FXML
+    private void clickButton3(ActionEvent actionEvent) {
+        labelForButtons.setText("Clicked Button3");
+    }
+}
