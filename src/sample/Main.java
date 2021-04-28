dd177d31c733778db53805cf871d16a69ce34b88
aytal <kitecat.kitekat@gmail.com>
2021-04-28 09:10:57 +0900

    Сделал пример 3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,10 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,18 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        Text text = new Text("Hello from JavaFX!");
-        text.setLayoutY(80);
-        text.setLayoutX(100);
-        Group group = new Group(text);
-        Scene scene = new Scene(group);
-        primaryStage.setScene(scene);
-
         primaryStage.setTitle("Hello World");
-        primaryStage.setWidth(300);
-        primaryStage.setHeight(250);
-
+        primaryStage.setScene(new Scene(root, 400, 200));
         primaryStage.show();
     }
 
